e135495ae8c1ee57ac421835d06a71ed982c7521
myriam.benali3@gmail.com <myriam.benali3@gmail.com>
2020-10-09 08:25:28 +0200

    Working on authentification

---
+++ src/main/java/com/polytech/mtonairserver/controller/AuthentificationController.java
@@ -0,0 +1,44 @@
+package com.polytech.mtonairserver.controller;
+
+import com.google.common.hash.Hashing;
+import com.polytech.mtonairserver.domains.AuthForm;
+import com.polytech.mtonairserver.domains.UserEntity;
+import com.polytech.mtonairserver.dto.LoginResponseDto;
+import com.polytech.mtonairserver.repositories.UserEntityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.nio.charset.StandardCharsets;
+
+public class AuthentificationController {
+
+    private UserEntityRepository userEntityRepository;
+
+    @Autowired
+    public AuthentificationController(UserEntityRepository userEntityRepository) {
+        this.userEntityRepository = userEntityRepository;
+    }
+
+    @RequestMapping(value = "/email", method = RequestMethod.POST)
+    public LoginResponseDto loginUser(AuthForm authForm) {
+
+        UserEntity userEntity = userEntityRepository.findByEmail(authForm.getEmail());
+        String hashedPassword = Hashing.sha256().hashString(authForm.getPassword(), StandardCharsets.UTF_8).toString();
+
+        if (!hashedPassword.equals(userEntity.getPassword())) {
+            // throw new ... une exception
+        }
+
+
+            LoginResponseDto loginResponseDto = new LoginResponseDto();
+            loginResponseDto.setUser(userEntity);
+            loginResponseDto.setToken("blabla");
+            return loginResponseDto;
+
+    }
+
+}
--- src/main/java/com/polytech/mtonairserver/repositories/UserEntityRepository.java
+++ src/main/java/com/polytech/mtonairserver/repositories/UserEntityRepository.java
@@ -2,10 +2,16 @@ package com.polytech.mtonairserver.repositories;
 
 import com.polytech.mtonairserver.domains.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public interface UserEntityRepository extends JpaRepository<UserEntity, Integer> {
     List<UserEntity> findAll();
     UserEntity findByIdUser(int id);
+    //Optional<UserEntity> findByEmail(String email);
+    Boolean existsByEmail(String email);
+    UserEntity findByEmail(String email);
 }
